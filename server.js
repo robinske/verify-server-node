f466d79f8b44d0c636657c9bae282ce3b9a0d284
Kelley Robinson <krobinson@twilio.com>
2019-10-02 14:28:09 -0700

    It would help if I capitalized PORT

--- server.js
+++ server.js
@@ -51,6 +51,6 @@ app.get('/', (req, res) => {
   });
 });
 
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Express server listening on port 3000.');
 });
